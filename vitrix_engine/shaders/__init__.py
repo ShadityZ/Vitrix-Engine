aa78eedd8f11743bfd25967d027b4aacbede9738
ShadityZ <kanjpatel33@gmail.com>
2022-05-11 19:14:59 +1000

    Few engine build bugs

--- vitrix_engine/shaders/__init__.py
+++ vitrix_engine/shaders/__init__.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from ursina.shaders.unlit_shader import unlit_shader
-from ursina.shaders.lit_with_shadows_shader import lit_with_shadows_shader
-from ursina.shaders.normals_shader import normals_shader
-from ursina.shaders.basic_lighting_shader import basic_lighting_shader
-from ursina.shaders.triplanar_shader import triplanar_shader
-from ursina.shaders.matcap_shader import matcap_shader
-from ursina.shaders.colored_lights_shader import colored_lights_shader
-from ursina.shaders.transition_shader import transition_shader
-=======
 from vitrix_engine.shaders.unlit_shader import unlit_shader
 from vitrix_engine.shaders.lit_with_shadows_shader import lit_with_shadows_shader
 from vitrix_engine.shaders.normals_shader import normals_shader
@@ -16,21 +6,12 @@ from vitrix_engine.shaders.triplanar_shader import triplanar_shader
 from vitrix_engine.shaders.matcap_shader import matcap_shader
 from vitrix_engine.shaders.colored_lights_shader import colored_lights_shader
 from vitrix_engine.shaders.transition_shader import transition_shader
->>>>>>> 9219278 (Big engine stuff)
 
 
 
 # post processing effects
-<<<<<<< HEAD
-from ursina.shaders.screenspace_shaders.camera_vertical_blur import camera_vertical_blur_shader
-from ursina.shaders.screenspace_shaders.camera_contrast import camera_contrast_shader
-from ursina.shaders.screenspace_shaders.camera_grayscale import camera_grayscale_shader
-from ursina.shaders.screenspace_shaders.fxaa import fxaa_shader
-from ursina.shaders.screenspace_shaders.ssao import ssao_shader
-=======
 from vitrix_engine.shaders.screenspace_shaders.camera_vertical_blur import camera_vertical_blur_shader
 from vitrix_engine.shaders.screenspace_shaders.camera_contrast import camera_contrast_shader
 from vitrix_engine.shaders.screenspace_shaders.camera_grayscale import camera_grayscale_shader
 from vitrix_engine.shaders.screenspace_shaders.fxaa import fxaa_shader
 from vitrix_engine.shaders.screenspace_shaders.ssao import ssao_shader
->>>>>>> 9219278 (Big engine stuff)
